227dfd6052a0000df9f55f0bfff5f42795465f25
Carlo Spader <carlo.spader@outlook.com>
2023-04-10 22:23:40 -0500

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+    using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTemplate.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("forecast", Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        var val = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+
+        return val;
+    }
+
+    [HttpGet("temperature", Name = "GetTemperature")]
+    public async Task<IEnumerable<WeatherForecast>> GetTemperature()
+    {
+        var @value = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+
+        return await Task.FromResult(@value);
+    }
+}
