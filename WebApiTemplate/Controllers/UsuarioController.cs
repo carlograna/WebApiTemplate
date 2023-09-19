c40947059b29f3093b2c2d26437746a345bfc4c1
CRISTHIAN <cristhian19cd@hotmail.com>
2023-09-19 14:57:39 -0500

    Implementado Nlog en el API

--- WebApiTemplate/Controllers/UsuarioController.cs
+++ WebApiTemplate/Controllers/UsuarioController.cs
@@ -12,13 +12,15 @@ namespace WebApiTemplate.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        private readonly ILogger<UsuarioController> _logger;
         private readonly IConfiguration _config;
 
-        public UsuarioController(IConfiguration config)
+        public UsuarioController(IConfiguration config, ILogger<UsuarioController> logger)
         {
             _config = config;
-            
+            _logger = logger;
         }
+    
 
         [HttpPost("loginJWT")]
         public IActionResult LoginJwt(LoginRequest userLogin)
@@ -28,8 +30,10 @@ namespace WebApiTemplate.Controllers
             {
                 // Crear el token
                 var token = Generate(user);
+                _logger.LogInformation($"Inicio de sesión exitoso para el usuario: {userLogin.Username}");
                 return Ok(token);
             }
+            _logger.LogWarning($"Intento de inicio de sesión fallido para el usuario: {userLogin.Username}");
             return NotFound("Usuario no encontrado");
         }
         private Usuario Authenticate(LoginRequest userLogin)
--- WebApiTemplate/Program.cs
+++ WebApiTemplate/Program.cs
@@ -1,49 +1,70 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+try
 {
-    options.RequireHttpsMetadata = false;
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters()
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
     {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-});
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception e)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}   
+    logger.Error(e, "Programa detenido porque tiene excepciones");
+    throw;
+}
 
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+finally
+{
+    NLog.LogManager.Shutdown();
+}
